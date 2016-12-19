2293dfa00468ee4271d701e2296231f95c7a4c22
Vyacheslav <digitect@live.ru>
2016-12-20 00:14:14 +0300

    Исправил ошибку в команде смены директории. Теперь она может при необходимости перейти на другой диск.

--- relanotes.bat
+++ relanotes.bat
@@ -8,8 +8,9 @@ REM @echo cd = %cd%
 REM Папка, в которой находится скрипт
 REM @echo dp0 = %~dp0
 
-REM Переходим в папку, в которой находится скрипт и основной запускаемый Питон-файл
-cd %~dp0
+REM Переходим в папку, в которой находится скрипт и основной запускаемый Питон-файл.
+REM Также включен флаг смены диска, если надо.
+cd /d %~dp0
 REM Запускаем основной исполняемый файл
 python relanotes.py
 REM c:\python35\python.exe D:\Dropbox\Projects\Relanotes\Relanotes-qt5-0.03\relanotes.py
