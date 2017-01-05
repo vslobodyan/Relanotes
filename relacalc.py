ddf3cf3cc54b9ba56aaf67554042c036919f2028
Vyacheslav <digitect@live.ru>
2017-01-05 04:19:01 +0300

    Исправил баг в главном запускающем файле RelaCalc. Теперь он нормально запускается как самостоятельная программа, перенесенная под PyQT5

--- relacalc.py
+++ relacalc.py
@@ -3,7 +3,7 @@
 
 import sys
 
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 #from PyQt4.QtCore import *
 #from PyQt4.QtGui import *
 
@@ -11,7 +11,8 @@ from src import calculator
 
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    # app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
 
     calculator_win = calculator.CalculatorWindow()
     calculator_win.show()
