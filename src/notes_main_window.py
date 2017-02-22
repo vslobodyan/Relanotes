73f137c7c1f0b75d958785c717d6618a612fef53
Вячеслав <digitect@live.ru>
2017-02-22 15:34:48 +0300

    Пробный вариант с изменением отступа параграфов для заголовков.

--- src/notes_main_window.py
+++ src/notes_main_window.py
@@ -2110,19 +2110,19 @@ class Note():
         # html_source = re.sub('== (.*?) ==', '<font id=head5>\\1</font>', html_source)
         # html_source = re.sub('= (.*?) =', '<font id=head6>\\1</font>', html_source)
 
-        html_source = re.sub('====== (.*?) ======', '<font id=head1>\\1</font>', html_source)
-        html_source = re.sub('===== (.*?) =====', '<font id=head2>\\1</font>', html_source)
-        html_source = re.sub('==== (.*?) ====', '<font id=head3>\\1</font>', html_source)
-        html_source = re.sub('=== (.*?) ===', '<font id=head4>\\1</font>', html_source)
-        html_source = re.sub('== (.*?) ==', '<font id=head5>\\1</font>', html_source)
-        html_source = re.sub('= (.*?) =', '<font id=head6>\\1</font>', html_source)
+        #html_source = re.sub('====== (.*?) ======', '<font id=head1>\\1</font>', html_source)
+        #html_source = re.sub('===== (.*?) =====', '<font id=head2>\\1</font>', html_source)
+        #html_source = re.sub('==== (.*?) ====', '<font id=head3>\\1</font>', html_source)
+        #html_source = re.sub('=== (.*?) ===', '<font id=head4>\\1</font>', html_source)
+        #html_source = re.sub('== (.*?) ==', '<font id=head5>\\1</font>', html_source)
+        #html_source = re.sub('= (.*?) =', '<font id=head6>\\1</font>', html_source)
 
-        #html_source = re.sub('====== (.*?) ======', '<div id=head1>\\1</div>', html_source)
-        #html_source = re.sub('===== (.*?) =====', '<div id=head2>\\1</div>', html_source)
-        #html_source = re.sub('==== (.*?) ====', '<div id=head3>\\1</div>', html_source)
-        #html_source = re.sub('=== (.*?) ===', '<div id=head4>\\1</div>', html_source)
-        #html_source = re.sub('== (.*?) ==', '<div id=head5>\\1</div>', html_source)
-        #html_source = re.sub('= (.*?) =', '<div id=head6>\\1</div>', html_source)
+        html_source = re.sub('\n====== (.*?) ======\n', '<div id=head1>\\1</div>', html_source)
+        html_source = re.sub('\n===== (.*?) =====\n', '<div id=head2>\\1</div>', html_source)
+        html_source = re.sub('\n==== (.*?) ====\n', '<div id=head3>\\1</div>', html_source)
+        html_source = re.sub('\n=== (.*?) ===\n', '<div id=head4>\\1</div>', html_source)
+        html_source = re.sub('\n== (.*?) ==\n', '<div id=head5>\\1</div>', html_source)
+        html_source = re.sub('\n= (.*?) =\n', '<div id=head6>\\1</div>', html_source)
 
 
         
