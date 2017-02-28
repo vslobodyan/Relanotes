f1508a9d091a8e21895d88f13d4071de0fe4c1d6
Вячеслав <digitect@live.ru>
2017-02-28 19:12:39 +0300

    Тестирование работает в 3 вариантах. Лишняя отладка из консоли по позиции курсора убрана.

--- src/notes_main_window.py
+++ src/notes_main_window.py
@@ -887,7 +887,7 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
         filename = main_window.current_open_note_link
         if filename:
             current_position = main_window.textBrowser_Note.textCursor().position()
-            print('Файл открытой заметки %s и позиция курсора %s' % (filename, current_position))
+            #print('Файл открытой заметки %s и позиция курсора %s' % (filename, current_position))
             # Если есть - сохраняем для неё последнюю позицию курсора
 
             # Обновляем запись в базе
@@ -4264,80 +4264,58 @@ class App_Tests():
             app_settings.settings.setValue('path_to_notes_convertation_test', self.path_to_notes_convertation)
             app_settings.settings.sync()
     
-                            
-    def notes_convertation_for_notelist(self):
-        # Выполняем тест для текущего списка заметок
-        self.collect_items(from_notelist=True)
-        pass
+                                
 
-    def notes_convertation_for_directory(self, change_path=False):
-        # Выполняем тест для директории из настроек
-        self.collect_items(from_directory=True, change_path=change_path)
-        pass
-
-
-    def notes_convertation(self, change_path=False):
+    def test_notes_items_for_convertation(self):
         # Тестовая функция, позволяющая проверить корректность конвертации форматирования при открытии и сохранении заметок
         print('Запускаем функцию тестирования конвертации форматирования при открытии и сохранении заметок')
 
-        # Диалог выбора пути для сканирования
-        if change_path or not self.path_to_notes_convertation:
-            print('Предлагаем смену каталога для теста')
-            new_path = give_correct_path_under_win_and_other(QtWidgets.QFileDialog.getExistingDirectory(main_window, "Select Directory with your Notes for Test", '' , QtWidgets.QFileDialog.ShowDirsOnly))
-            print('path_to_notes_convertation: ##%s##' % new_path)
-            #return 0
-            if not new_path:
-                print('Каталог не выбран.')
-                return 0
+        for filename in self.items:
+            fileObj = codecs.open(filename, "r", "utf-8")
+            original_text = fileObj.read()
+            fileObj.close()
+
+            # Загружаем файл в окно редактора
+            main_window.open_file_in_editor(filename)
+
+            # Конвертируем zim text в html для редактора
+            html_source = note.convert_zim_text_to_html_source(original_text)
+            # Устанавливаем html-исходник для редактора
+            main_window.doc_source.setHtml(html_source)
+            main_window.textBrowser_Note.setDocument(main_window.doc_source)
+
+
+            # Конвертируем файл как-бы для сохранения на диск
+            note_source = main_window.textBrowser_Note.toHtml()
+            saved_text = note.convert_html_source_to_zim_text(note_source)
+
+            # Сравниваем оригинал и "сохраненный" вариант
+            diff_result = get_diff_text(original_text, saved_text, filename, filename + '-saved')
+            if diff_result:
+                print()
+                # print('Результат сравнения:')
+                print(diff_result)
+                # for line in diff_result:
+                #    print(line)
             else:
-                print('Выбран новый каталог для тестов: %s' % new_path)
-                self.update_path_info_for_notes_convertation(new_path, save=True)
-        else:
-            print('Готовим тест без смены каталога')
-        #return 0
-        #if not app_settings.path_to_notes:
-        #    app_settings.path_to_notes = "D:\Test\\Notes"
-        print('Пользователь выбрал для теста каталог %s' % self.path_to_notes_convertation)
-
-        for root, dirs, files in os.walk(self.path_to_notes_convertation):
-            for file in files:
-                if file.endswith('.txt'):
-                    filename = os.path.join(root, file)
-                    # Читаем файл в память
-                    fileObj = codecs.open(filename, "r", "utf-8")
-                    original_text = fileObj.read()
-                    fileObj.close()
-
-                    # Загружаем файл в окно редактора
-                    main_window.open_file_in_editor(filename)
-
-                    # Конвертируем zim text в html для редактора
-                    html_source = note.convert_zim_text_to_html_source(original_text)
-                    # Устанавливаем html-исходник для редактора
-                    main_window.doc_source.setHtml(html_source)
-                    main_window.textBrowser_Note.setDocument(main_window.doc_source)
-
-
-                    # Конвертируем файл как-бы для сохранения на диск
-                    note_source = main_window.textBrowser_Note.toHtml()
-                    saved_text = note.convert_html_source_to_zim_text(note_source)
-
-                    # Сравниваем оригинал и "сохраненный" вариант
-                    diff_result = get_diff_text(original_text, saved_text, filename, filename + '-saved')
-                    if diff_result:
-                        print()
-                        # print('Результат сравнения:')
-                        print(diff_result)
-                        # for line in diff_result:
-                        #    print(line)
-                    else:
-                        print('.', end="", flush=True)
+                print('.', end="", flush=True)
 
         print()
         print('Тестирование завершено.')
 
+    def notes_convertation_for_notelist(self):
+        # Выполняем тест для текущего списка заметок
+        self.collect_items(from_notelist=True)
+        self.test_notes_items_for_convertation()
+
+    def notes_convertation_for_directory(self, change_path=False):
+        # Выполняем тест для директории из настроек
+        self.collect_items(from_directory=True, change_path=change_path)
+        self.test_notes_items_for_convertation()
+
     def notes_convertation_change_path(self):
         self.notes_convertation_for_directory(change_path=True)
+
     
     def __init__(self):
         print('Инициализация класса тестов')
