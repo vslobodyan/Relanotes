f48fd9247937953dc16cf27ece078917fb4d8ff7
vyacheslav <digitect@live.ru>
2017-10-22 17:46:39 +0300

    Вывел в командном файле отладочную печать, как он (файл) пытается получить свое местоположение.

--- relanotes.sh
+++ relanotes.sh
@@ -1,6 +1,7 @@
 ﻿#!/bin/bash
 # Получаем путь к каталогу со скриптом и основным запускаемым файлом
 CURRENT_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+echo "CURRENT_FILE_DIR=$CURRENT_FILE_DIR"
 # Переходим в него
 cd $CURRENT_FILE_DIR
 
