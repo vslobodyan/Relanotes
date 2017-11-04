247bf5b639bddd852f5f33197c52d0a9e112a11d
vyacheslav <digitect@live.ru>
2017-11-04 17:52:34 +0300

    Добавил скрипт установки симлинка на файл запуска для Реланотс в Линуксе.

---
+++ relanotes.sh-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_NAME="relanotes.sh"
+SCRIPT_PATH=`pwd`
+LINK_PATH="/usr/local/bin"
+LINK_NAME="relanotes"
+SCRIPT_FILENAME="$SCRIPT_PATH/relanotes.sh"
+LINK_FROM="$LINK_PATH/$LINK_NAME"
+LINK_TO="$SCRIPT_PATH/$SCRIPT_NAME"
+echo "Создаем ссылку из $LINK_FROM на файл $LINK_TO"
+sudo ln -s $LINK_TO $LINK_FROM
